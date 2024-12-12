09873cf79833f315e859aa044edca3ea4bff621b
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-12-12 14:16:01 -0300

    Update sidebar validator

--- middlewares/validators/groupValidator.js
+++ middlewares/validators/groupValidator.js
@@ -16,21 +16,20 @@ exports.validateGroupCreation = [
 ];
 
 exports.validateGroupUpdate = [
-    body('name')
-      .trim()
-      .isLength({ min: 1, max: 32 })
-      .withMessage('Name must be between 1 and 32 characters.')
-      .escape(),
-    body('description')
-      .trim()
-      .isLength({ min: 1, max: 256 })
-      .withMessage('Description must be between 1 and 256 characters.')
-      .escape(),
-    body('sidebar')
-      .trim()
-      .isLength({ min: 1, max: 256 })
-      .withMessage('Sidebar must be between 1 and 256 characters.')
-      .escape(),    
-    handleValidationError,
-  ];
-  
+  body('name')
+    .trim()
+    .isLength({ min: 1, max: 32 })
+    .withMessage('Name must be between 1 and 32 characters.')
+    .escape(),
+  body('description')
+    .trim()
+    .isLength({ min: 1, max: 256 })
+    .withMessage('Description must be between 1 and 256 characters.')
+    .escape(),
+  body('sidebar')
+    .trim()
+    .isLength({ min: 0, max: 256 })
+    .withMessage('Sidebar must be between 0 and 256 characters.')
+    .escape(),
+  handleValidationError,
+];
